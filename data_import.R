f615ac59814237e2c0f51b1debdd11070728abe4
Ariel Guerra Adames <arielo.guerraa@gmail.com>
2023-03-08 11:41:28 +0100

    Update path names.

--- data_import.R
+++ data_import.R
@@ -1,8 +1,8 @@
 ## DATA IMPORT SCRIPT ##
 ## This script imports tables of interest from the MIMIC-IV dataset ##
 
-patients <- read.csv("/Users/arielguerra/Documents/MLDM/Data Mining/Project/mimic-iv-2.2/hosp/patients.csv")
-diagnoses <- read.csv("/Users/arielguerra/Documents/MLDM/Data Mining/Project/mimic-iv-2.2/hosp/diagnoses_icd.csv")
-procedures <- read.csv("/Users/arielguerra/Documents/MLDM/Data Mining/Project/mimic-iv-2.2/hosp/procedures_icd.csv")
-hosp_cost <- read.csv("/Users/arielguerra/Documents/MLDM/Data Mining/Project/mimic-iv-2.2/hosp/hcpcsevents.csv")
-omr <- read.csv("/Users/arielguerra/Documents/MLDM/Data Mining/Project/mimic-iv-2.2/hosp/emar.csv")
+patients <- read.csv("C:/Users/ariel/Documents/MLDM/Data Mining/mimic-iv-2.2/hosp/patients.csv")
+diagnoses <- read.csv("C:/Users/ariel/Documents/MLDM/Data Mining/mimic-iv-2.2/hosp/diagnoses_icd.csv")
+procedures <- read.csv("C:/Users/ariel/Documents/MLDM/Data Mining/mimic-iv-2.2/hosp/procedures_icd.csv")
+hosp_cost <- read.csv("C:/Users/ariel/Documents/MLDM/Data Mining/mimic-iv-2.2/hosp/hcpcsevents.csv")
+omr <- read.csv("C:/Users/ariel/Documents/MLDM/Data Mining/mimic-iv-2.2/hosp/emar.csv")
