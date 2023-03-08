da883ba932d52b36ba0eac401ce584634f33ac9d
Ariel Guerra-Adames <arielo.guerraa@gmail.com>
2023-03-08 11:22:16 +0100

    Update data_import.R

--- data_import.R
+++ data_import.R
@@ -1,4 +1,8 @@
 ## DATA IMPORT SCRIPT ##
 ## This script imports tables of interest from the MIMIC-IV dataset ##
 
-
+patients <- read.csv("/Users/arielguerra/Documents/MLDM/Data Mining/Project/mimic-iv-2.2/hosp/patients.csv")
+diagnoses <- read.csv("/Users/arielguerra/Documents/MLDM/Data Mining/Project/mimic-iv-2.2/hosp/diagnoses_icd.csv")
+procedures <- read.csv("/Users/arielguerra/Documents/MLDM/Data Mining/Project/mimic-iv-2.2/hosp/procedures_icd.csv")
+hosp_cost <- read.csv("/Users/arielguerra/Documents/MLDM/Data Mining/Project/mimic-iv-2.2/hosp/hcpcsevents.csv")
+omr <- read.csv("/Users/arielguerra/Documents/MLDM/Data Mining/Project/mimic-iv-2.2/hosp/emar.csv")
